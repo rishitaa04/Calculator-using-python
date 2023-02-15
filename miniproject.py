741efaefac23d5ea6652847dab0c541dfa8e34cf
Rishita <125037556+rishitaa04@users.noreply.github.com>
2023-02-16 00:12:06 +0530

    Add files via upload

---
+++ miniproject.py
@@ -0,0 +1,23 @@
+ # BUILDING CALCULATOR 
+ 
+first = int(input("Enter first number : "))
+operator = input(" Enter operator +,-,*,/,% : ")
+second = int(input("Enter second number : "))
+ 
+if operator == '+' : 
+    print(first + second)
+    
+elif operator == "-" :
+    print(first - second)
+    
+elif operator == "*" :
+    print(first * second)
+    
+elif operator == "/" :
+    print(first / second)
+    
+elif operator == "%" :
+    print(first % second)
+
+else :
+    print("Invalid operaton")
